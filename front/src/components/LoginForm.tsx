5f6ef0d6dbaff53ce2dbe3db3fde70d2437bf8e3
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-08-08 18:56:14 +0300

    react hook form connected

---
+++ front/src/components/LoginForm.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+ email: string;
+ password: string;
+}
+
+const LoginForm: FC = () => {
+ const { register, handleSubmit } = useForm<FormData>();
+ const onSubmit = (data: { email: string; password: string }) => {
+  alert(JSON.stringify(data));
+ };
+ return (
+  <form
+   onSubmit={handleSubmit(onSubmit)}
+   className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-1/3 bg-slate-50 flex items-center p-5 flex-col gap-5"
+  >
+   <input
+    {...register("email")}
+    className="border-slate-900 border-2 w-full rounded-md p-2 text-2xl"
+   />
+   <input
+    {...register("password")}
+    className="border-slate-900 border-2 w-full rounded-md p-2 text-2xl"
+   />
+   <input
+    type="submit"
+    value="Log In"
+    className="w-full text-center py-2 text-2xl font-semibold bg-gradient-to-r from-violet-500 to-fuchsia-500 text-slate-50 rounded-xl cursor-pointer hover:scale-105 duration-300"
+   />
+  </form>
+ );
+};
+
+export default LoginForm;
--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,11 +1,63 @@
-import { FC } from "react";
+import { LogIn } from "lucide-react";
+import { FC, useState } from "react";
+import LoginForm from "./LoginForm";
 
 const Navbar: FC = () => {
+ const [isLogIn, setIsLogIn] = useState<boolean>(false);
+
  return (
-  <div className="w-full flex justify-center bg-gradient-to-r from-violet-500 to-fuchsia-500 text-8xl uppercase py-10 font-black font-outline-4 text-slate-50/50">
-   <div className="absolute w-full h-full top-0 left-0 bg-slate-900/50 -z-10"></div>
-   Veldom
-  </div>
+  <>
+   {isLogIn && (
+    <div className="absolute w-screen h-screen">
+     <div
+      className="w-screen h-screen bg-slate-900/70 cursor-pointer absolute z-40"
+      onClick={() => setIsLogIn(false)}
+     ></div>
+     <LoginForm />
+    </div>
+   )}
+   <div className="w-full flex justify-around bg-gradient-to-r from-violet-500 to-fuchsia-500 text-8xl uppercase py-10 text-slate-50/50">
+    <div className="absolute w-full h-full top-0 left-0 bg-slate-900/50 -z-10"></div>
+    <a href="/" className="font-black font-outline-4 cursor-pointer">
+     Veldom
+    </a>
+    <div className="flex gap-10 items-center">
+     <a href="https://t.me/velikiyrestring">
+      <svg
+       className="rounded-xl cursor-pointer hover:scale-110 duration-300"
+       fill="#FFFFFF"
+       height="50px"
+       width="50px"
+       version="1.1"
+       id="Layer_1"
+       xmlns="http://www.w3.org/2000/svg"
+       viewBox="0 0 455 455"
+      >
+       <g>
+        <path
+         d="M0,0v455h455V0H0z M384.814,100.68l-53.458,257.136
+		c-1.259,6.071-8.378,8.822-13.401,5.172l-72.975-52.981c-4.43-3.217-10.471-3.046-14.712,0.412l-40.46,32.981
+		c-4.695,3.84-11.771,1.7-13.569-4.083l-28.094-90.351l-72.583-27.089c-7.373-2.762-7.436-13.171-0.084-16.003L373.36,90.959
+		C379.675,88.517,386.19,94.049,384.814,100.68z"
+        />
+        <path
+         d="M313.567,147.179l-141.854,87.367c-5.437,3.355-7.996,9.921-6.242,16.068
+		l15.337,53.891c1.091,3.818,6.631,3.428,7.162-0.517l3.986-29.553c0.753-5.564,3.406-10.693,7.522-14.522l117.069-108.822
+		C318.739,149.061,316.115,145.614,313.567,147.179z"
+        />
+       </g>
+      </svg>
+     </a>
+     <button
+      onClick={() => setIsLogIn(true)}
+      className="flex gap-2 items-center text-4xl font-semibold text-slate-50 hover:scale-110 duration-300"
+     >
+      <LogIn size={50} />
+      Login
+     </button>
+    </div>
+   </div>
+  </>
  );
 };
 
